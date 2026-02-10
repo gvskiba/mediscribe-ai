f22bc791774bc95e2fb12151d1a53e356ea61857
gabriel.skiba <gabriel.skiba@gmail.com>
2026-02-10 23:48:30 +0000

    File changes

--- src/pages/NoteDetail.jsx
+++ src/pages/NoteDetail.jsx
@@ -1044,9 +1044,7 @@ Generated: ${new Date().toLocaleString()}
                  </Button>
                </div>
              )}
-             <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
-               {/* Main Content - Left Column (2/3) */}
-               <div className="lg:col-span-2 space-y-6">
+             <div className="space-y-6">
                  <StructuredNotePreview 
                    note={note} 
                    onUpdate={(field, value) => {

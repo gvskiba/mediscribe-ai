0c11e4f295dfde9745aa877cb8bd45cb9c3874ab
gabriel.skiba <gabriel.skiba@gmail.com>
2026-02-10 03:20:18 +0000

    File changes

--- src/pages/NoteDetail.jsx
+++ src/pages/NoteDetail.jsx
@@ -629,10 +629,7 @@ Generated: ${new Date().toLocaleString()}
                Guidelines & Codes
              </TabsTrigger>
              <TabsTrigger value="imaging" className="rounded-none border-b-2 border-transparent data-[state=active]:border-blue-600 px-3">
-               Imaging
-             </TabsTrigger>
-             <TabsTrigger value="labs" className="rounded-none border-b-2 border-transparent data-[state=active]:border-blue-600 px-3">
-               Labs
+               Result Analysis
              </TabsTrigger>
              <TabsTrigger value="metadata" className="rounded-none border-b-2 border-transparent data-[state=active]:border-blue-600 px-3">
                Metadata
